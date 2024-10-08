fbeeadc483fac16ccf53ee3733dfd3f9e77ede1a
m4tu4g <71326926+m4tu4g@users.noreply.github.com>
2024-10-08 16:39:07 +0530

    feat: test Cassandra as Sink

--- src/main/java/com/shortbin/cambridge_service/KafkaToKafka.java
+++ src/main/java/com/shortbin/cambridge_service/KafkaToKafka.java
@@ -1,6 +1,12 @@
 package com.shortbin.cambridge_service;
 
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.QueryOptions;
+import com.datastax.driver.core.SocketOptions;
+import com.datastax.driver.mapping.Mapper;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
 import org.apache.flink.connector.kafka.sink.KafkaSink;
@@ -8,6 +14,13 @@ import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.connectors.cassandra.CassandraSink;
+import org.apache.flink.streaming.connectors.cassandra.ClusterBuilder;
+
+import java.net.InetSocketAddress;
+
 
 public class KafkaToKafka {
 
@@ -41,6 +54,47 @@ public class KafkaToKafka {
                 .build();
 
         stream.sinkTo(sink);
+
+        DataStream<WordCount> result = stream
+                .flatMap((FlatMapFunction<String, WordCount>) (value, out) -> {
+                    // normalize and split the line
+                    String[] words = value.toLowerCase().split("\\s");
+
+                    // emit the pairs
+                    for (String word : words) {
+                        if (!word.isEmpty()) {
+                            //Do not accept empty word, since word is defined as primary key in C* table
+                            out.collect(new WordCount(word, 1L));
+                        }
+                    }
+                })
+                .keyBy(WordCount::getWord)
+                .window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
+                .reduce(new ReduceFunction<WordCount>() {
+                    @Override
+                    public WordCount reduce(WordCount a, WordCount b) {
+                        return new WordCount(a.getWord(), a.getCount() + b.getCount());
+                    }
+                });
+
+        ClusterBuilder cassClusterBuilder = new ClusterBuilder() {
+            @Override
+            protected Cluster buildCluster(Cluster.Builder builder) {
+                return builder.addContactPointsWithPorts(new InetSocketAddress("ampere_2", 9042))
+                        .withQueryOptions(new QueryOptions())
+                        .withSocketOptions(new SocketOptions()
+                                .setConnectTimeoutMillis(50000)
+                                .setReadTimeoutMillis(50000))
+                        .withCredentials("cassandra", "cassandra")
+                        .build();
+            }
+        };
+
+        CassandraSink.addSink(result)
+                .setClusterBuilder(cassClusterBuilder)
+                .setMapperOptions(() -> new Mapper.Option[]{Mapper.Option.saveNullFields(true)})
+                .build()
+                .name("Cassandra Sink");
 
         env.execute("KafkaToKafka");
     }
---
+++ src/main/java/com/shortbin/cambridge_service/WordCount.java
@@ -0,0 +1,43 @@
+package com.shortbin.cambridge_service;
+
+import com.datastax.driver.mapping.annotations.Column;
+import com.datastax.driver.mapping.annotations.Table;
+
+@Table(keyspace = "example", name = "wordcount")
+public class WordCount {
+
+    @Column(name = "word")
+    private String word = "";
+
+    @Column(name = "count")
+    private long count = 0;
+
+    public WordCount() {
+    }
+
+    public WordCount(String word, long count) {
+        this.setWord(word);
+        this.setCount(count);
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public long getCount() {
+        return count;
+    }
+
+    public void setCount(long count) {
+        this.count = count;
+    }
+
+    @Override
+    public String toString() {
+        return getWord() + " : " + getCount();
+    }
+}
