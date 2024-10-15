9a5c9a9b7a553440c0777fe1bde25c86bc6a932c
m4tu4g <71326926+m4tu4g@users.noreply.github.com>
2024-10-15 15:36:48 +0530

    feat: sink processed data to postgres

--- src/main/java/com/shortbin/cambridge_service/ClickAnalytics.java
+++ src/main/java/com/shortbin/cambridge_service/ClickAnalytics.java
@@ -6,21 +6,30 @@ import com.datastax.driver.core.SocketOptions;
 import com.datastax.driver.mapping.Mapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.shortbin.cambridge_service.model.Click;
+import com.shortbin.cambridge_service.model.ClickAggregate;
 import com.shortbin.cambridge_service.model.ClickEvent;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.connector.jdbc.JdbcConnectionOptions;
+import org.apache.flink.connector.jdbc.JdbcExecutionOptions;
+import org.apache.flink.connector.jdbc.JdbcSink;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.windowing.WindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
 import org.apache.flink.streaming.connectors.cassandra.CassandraSink;
 import org.apache.flink.streaming.connectors.cassandra.ClusterBuilder;
 
 import java.net.InetSocketAddress;
+import java.sql.PreparedStatement;
 
 
-public class KafkaToCassandra {
+public class ClickAnalytics {
 
     public static void main(String[] args) throws Exception {
 
@@ -84,7 +93,54 @@ public class KafkaToCassandra {
                 .build()
                 .name("Cassandra Sink");
 
+        DataStream<ClickAggregate> aggregatedStream = enhancedStream
+//                .assignTimestampsAndWatermarks(
+//                        WatermarkStrategy.<Click>forBoundedOutOfOrderness(Duration.ofSeconds(5))
+//                                .withTimestampAssigner((event, timestamp) -> event.getTimestamp().getTime())
+//                )
+                .keyBy(Click::getShortID)
+                .window(TumblingProcessingTimeWindows.of(Time.seconds(10)))
+                .apply((WindowFunction<Click, ClickAggregate, String, TimeWindow>) (shortId, window, input, out) -> {
+                    Integer count = 0;
+//                    Map<String, Integer> countryCounter = new HashMap<>();
+//                    Map<String, Integer> referrerCounter = new HashMap<>();
 
-        env.execute("KafkaToCassandra");
+                    for (Click event : input) {
+                        count++;
+//                        countryCounter.merge(event.getCountry(), 1, Integer::sum);
+//                        referrerCounter.merge(event.getReferer(), 1, Integer::sum);
+                    }
+                    out.collect(new ClickAggregate(shortId, count));
+//                    System.out.println("Aggregating Clicks: " + shortId + " " + count);
+                })
+                .returns(ClickAggregate.class)
+                .name("Aggregated Clicks");
+
+        aggregatedStream.addSink(JdbcSink.sink(
+                        "INSERT INTO click_analytics (short_id, count) VALUES (?, ?) " +
+                                "ON CONFLICT (short_id) DO UPDATE SET " +
+                                "count = click_analytics.count + EXCLUDED.count;",
+                        (PreparedStatement ps, ClickAggregate clickAggregate) -> {
+                            ps.setString(1, clickAggregate.getShortId());
+                            ps.setInt(2, clickAggregate.getCount());
+//                            ps.setString(3, clickAggregate.getCountriesJson());
+//                            ps.setString(4, clickAggregate.getReferrersJson());
+//                            System.out.println("Inserting into click_analytics: " + clickAggregate.getShortId() + " " + clickAggregate.getCount());
+                        },
+                        JdbcExecutionOptions.builder()
+                                .withBatchIntervalMs(200)             // optional: default = 0, meaning no time-based execution is done
+//                                .withBatchSize(1000)                  // optional: default = 5000 values
+                                .withMaxRetries(5)                    // optional: default = 3
+                                .build(),
+                        new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
+                                .withUrl("jdbc:postgresql://ampere_2:5432/shortbin_dev")
+                                .withDriverName("org.postgresql.Driver")
+                                .withUsername("dev_user")
+                                .withPassword("jw8s0F4B5a")
+                                .build()
+                ))
+                .name("PostgreSQL Sink");
+
+        env.execute("ClickAnalytics");
     }
 }
